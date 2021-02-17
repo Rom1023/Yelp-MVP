a8f8d78b57fb99d2eb5b69c6a1346b186e7c75a8
romuald gaboriau <gaboriau.r@gmail.com>
2021-02-17 15:26:44 -0500

    Improved validations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,7 +13,6 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(params_restaurant)
-    # @restaurant.save
 
     if @restaurant.save
       redirect_to restaurant_path(@restaurant)
