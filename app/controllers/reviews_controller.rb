32d3ec5da08af014340d5fd093b92f708d0ea683
romuald gaboriau <gaboriau.r@gmail.com>
2021-02-17 13:59:33 -0500

    Added Models and Controllers

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(params_reviews)
+    @review.restaurant = @restaurant
+
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def params_reviews
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+jatoba = Restaurant.create(
+  {
+    name: 'Jatoba',
+    address: '1184 Place Phillips, Montréal, QC H3B 3C8',
+    category: 'japanese'
+  }
+)
+
+ile_flottante = Restaurant.create(
+  {
+    name: 'Île Flottante',
+    address: '176 Saint Viateur West Street, QC 2L3, Rue Saint- Viateur O,
+     Montreal, Quebec H2T 2L3',
+    category: 'french'
+  }
+)
+
+marconi = Restaurant.create(
+  {
+    name: 'Marconi',
+    address: '45 Avenue Mozart Ouest Montréal, QC H2S 1C1',
+    category: 'italian'
+  }
+)
+
+le_serpent = Restaurant.create(
+  {
+    name: 'Le Serpent',
+    address: '257 Rue Prince, Montréal, QC H3C 2N4',
+    category: 'italian'
+  }
+)
+
+helicoptere = Restaurant.create(
+  {
+    name: 'Hélicoptère',
+    address: '4255 Rue Ontario E, Montréal, QC H1V 1K4',
+    category: 'chinese'
+  }
+)
